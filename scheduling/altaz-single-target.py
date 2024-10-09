4f8ab1dbb74222b54aa21951202817afba3ce53b
Owen Johnson <60224822+OwenJohnsons@users.noreply.github.com>
2024-10-09 15:37:55 -0700

    Update altaz-single-target.py

--- scheduling/altaz-single-target.py
+++ scheduling/altaz-single-target.py
@@ -1,18 +1,17 @@
-#%% 
 '''
-Code Purpose: Plot elevation and sensitivity plots for a given target at a given obsevration window for IE613 
+Code Purpose: Plot elevation and sensitivity plots for a given target at a given observation window for IE613 
 Author: Owen A. Johnson
 Date: 2024-03-05
 '''
 
 import matplotlib.pyplot as plt
-import smplotlib
 import matplotlib.image as mpimg
 import matplotlib.dates as mdates
 import astropy.units as u
 import numpy as np
 import argparse
 import os 
+import scienceplots; plt.style.use(['science', 'no-latex'])
 
 from astropy.coordinates import EarthLocation, SkyCoord, solar_system_ephemeris, get_body, AltAz
 from astropy.time import Time
@@ -24,17 +23,17 @@ from datetime import datetime
 #          - Set up Arguments -
 # ------------------------------------
 parser = argparse.ArgumentParser(description='Plot elevation and sensitivity plots for a given target')
-parser.add_argument('--name', type=str, help='Name of the target', required=True, nargs='+')
-parser.add_argument('--ra', type=float, help='Right Ascension of the target in radians or degs', required=False)
-parser.add_argument('--dec', type=float, help='Declination of the target in radians', required=False)
-parser.add_argument('--date', help='Date of observation in form YYYY-MM-DD HH:MM:SS', required=True, nargs='+')
+parser.add_argument('--name', type=str, help='Name of the target', required=True)
+parser.add_argument('--date', help='Date of observation in form YYYY-MM-DD HH:MM:SS', default=datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
+parser.add_argument('ra', type=float, help='Right Ascension of the target in radians or degrees', nargs='?')
+parser.add_argument('dec', type=float, help='Declination of the target in radians or degrees', nargs='?')
 
 args = parser.parse_args()
 
 # --- Check if the date is in the correct format ---
 try:
-    observe_time = ' '.join(args.date)
-    trgt_name = ' '.join(args.name)
+    observe_time = args.date
+    trgt_name = args.name
     datetime.strptime(observe_time, '%Y-%m-%d %H:%M:%S')
 except ValueError:
     raise ValueError("Incorrect date format, should be YYYY-MM-DD HH:MM:SS")
@@ -94,7 +93,7 @@ observer = Observer(name='I-LOFAR',
                temperature=0 * u.deg_C,
                description="LOFAR Station IE613")
 
-observe_time = Time(' '.join(args.date)); print('Observation start time:', observe_time)
+observe_time = Time(args.date); print('Observation start time:', observe_time)
 obs_window = 31; print('Observation window:', obs_window, 'hours')
 increment = obs_window / 1000; print('Time increment:', increment, 'minutes')  # Divide the window into 1000 increments
 observe_times = observe_time + np.linspace(0, obs_window, 1000) * u.hour
@@ -121,10 +120,9 @@ sun_style = {'color': 'y'}
 
 # - Custom Target - 
 if trgt_name == 'Sun':
-    print('lads lads lads')
+    print('Sun selected, no need for custom target alt-az plotting.')
     alt = sun_coords.transform_to(AltAz(obstime=observe_times, location=location)).alt.degree
     az = sun_coords.transform_to(AltAz(obstime=observe_times, location=location)).az.degree
-    pass 
 else:
     coord_deg = SkyCoord(ra=ra_deg, dec=dec_deg)
     custom_target = FixedTarget(name=trgt_name, coord=coord_deg)
@@ -142,6 +140,7 @@ if trgt_name != 'Sun':
     plot_sky(custom_target, observer, observe_times, style_kwargs=custom_style)
 plt.legend(loc='center left', bbox_to_anchor=(1.05, 0.5))
 plt.savefig('sky-plot.png', dpi=200)
+plt.close()
 
 #%%
 plt.figure(figsize=(20, 8))
@@ -151,29 +150,76 @@ ax2 = plt.subplot(2, 2, 3, sharex=ax1)
 ax3 = plt.subplot(1, 2, 2)
 
 # Plotting azimuth
-ax1.plot(observe_times.datetime, az, label='Azimuth')
+ax1.plot(observe_times.datetime, az, label='Azimuth', color='black')
 ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
 ax1.xaxis.set_major_locator(mdates.HourLocator(interval=7))
 ax1.set_ylabel('Azimuth')
-ax1.legend()
+ax1.legend(frameon=True)
 # hide xticks 
 plt.setp(ax1.get_xticklabels(), visible=False)
 ax1.grid(True)
 
 # Plotting altitude
-ax2.plot(observe_times.datetime, alt, label='Altitude')
+
+# Find where altitude is max_alt - 10 on either side
+max_alt_index = np.where(alt == np.max(alt))[0][0]
+max_alt = np.max(alt)
+
+# Check if the max altitude is greater than 30 degrees
+if max_alt > 30:
+    print(f"Max altitude ({max_alt}) is greater than 30°, finding where it crosses 30° instead of max-10°.")
+
+    # Find where altitude crosses 30 degrees before and after the max altitude
+    try:
+        before_max_index = np.where(alt[:max_alt_index] <= 30)[0][-1]
+    except IndexError:
+        before_max_index = None
+        print("No altitude crossing below 30° before the max altitude")
+
+    try:
+        after_max_index = np.where(alt[max_alt_index:] <= 30)[0][0] + max_alt_index
+    except IndexError:
+        after_max_index = None
+        print("No altitude crossing below 30° after the max altitude")
+else:
+    print(f"Max altitude ({max_alt}) is less than or equal to 30°, using max-10° rule.")
+
+    # Initialize variables for before and after max
+    before_max_index, after_max_index = None, None
+
+    # Try to find the 10° drop points, handle out-of-bounds exceptions
+    try:
+        before_max_index = np.where(alt[:max_alt_index] <= max_alt - 10)[0][-1]
+    except IndexError:
+        print("No altitude drop by 10° before the max altitude")
+
+    try:
+        after_max_index = np.where(alt[max_alt_index:] <= max_alt - 10)[0][0] + max_alt_index
+    except IndexError:
+        print("No altitude drop by 10° after the max altitude")
+
+# Plot the altitude and vertical lines if the indices are found
+ax2.plot(observe_times.datetime, alt, label='Altitude', color='black')
+
+# plot max altitude time 
+ax2.axvline(observe_times.datetime[max_alt_index], label=f'Max Alt at {observe_times.datetime[max_alt_index].strftime("%H:%M")}', color='blue')
+if before_max_index is not None:
+    ax2.axvline(observe_times.datetime[before_max_index], color='g', linestyle='--', label=f'Crossed 30° before max at {observe_times.datetime[before_max_index].strftime("%H:%M")}')
+if after_max_index is not None:
+    ax2.axvline(observe_times.datetime[after_max_index], color='g', linestyle='--', label=f'Crossed 30° after max at {observe_times.datetime[after_max_index].strftime("%H:%M")}')
+    
 ax2.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
 ax2.xaxis.set_major_locator(mdates.HourLocator(interval=4))
-ax2.axhline(-11, color='r', linestyle='--', label='Horizon')
+ax2.axhline(0, color='r', linestyle='--', label='Horizon')
 ax2.set_xlabel('Time')
 ax2.set_ylabel('Altitude')
-ax2.legend()
+ax2.legend(frameon=True)
 ax2.grid(True)
 
 # Displaying image
 img = mpimg.imread('sky-plot.png')
-flipped_img = np.flipud(img)  # Flip image vertically
-ax3.imshow(flipped_img)
+# flipped_img = np.flipud(img)  # Flip image vertically
+ax3.imshow(img)
 ax3.axis('off')  # Hide axes for the image
 
 day_of_month = observe_time.datetime.day  # Get day of the month
@@ -192,51 +238,4 @@ date_string = f"{day_name}, {month}, {day_of_month}{day_suffix}"
 plt.suptitle('%s observation from IE613 starting %s' % (trgt_name, date_string), fontsize=16)
 plt.tight_layout()
 plt.savefig('./elevation-plots/%s-elevation-plot-%s.png' % (trgt_name, (str(day_of_month) + str(month))), dpi=200)
-plt.show()
-
-# --- Call David McKenna's Tsys Script ---
-import subprocess
-
-ra_rad = ra_deg.to(u.rad).value; dec_rad = dec_deg.to(u.rad).value
-command = 'python ./tsky_sefd_LOFAR_ilt.py --ra %s --dec %s --freqs 100 110 120 130 140 150 160 170 180 190' % (ra_rad[0], dec_rad[0])
-# command = 'python ./tsky_sefd_LOFAR_ilt.py --ra 4.8735655 --dec 0.67690307 --freqs 100 110 120 130 140 150 160 170 180 190'
-output = subprocess.check_output(command, shell=True).decode('utf-8')
-#%%
-freq = []; conv_temp = []; raw_temp = []; diff_temp = []
-lines = output.strip().split('\n')
-
-lofar_bandwidth = 3.66e6 # Hz
-tobs_v = 6*3600 # seconds
-
-for line in lines[4:]:
-    # Split each line by tabs
-    columns = line.split('\t')
-    # print(columns)
-    freq.append(float(columns[0].split(':')[0]))
-    print(columns[0].split(':')[0]) 
-    conv_temp.append(float(columns[2]))
-    raw_temp.append(float(columns[4]))
-    diff_temp.append(float(columns[6]))
-
-freq = np.array(freq); conv_temp = np.array(conv_temp); raw_temp = np.array(raw_temp); diff_temp = np.array(diff_temp)
-Aeff = np.linspace(2400, 1422, len(freq))
-
-def sens_limit(snr, tsys, Aeff, bandwidth, tobs): 
-    num = snr*tsys*1380*2
-    print(num)
-    dom = (Aeff*(np.sqrt(2*tobs*bandwidth)))  
-    print(dom)
-    return num/dom
-
-smin = sens_limit(1, conv_temp, Aeff, lofar_bandwidth, tobs_v)
-print('Sensitivity Limit (mJy):', 1000*smin.min())
-
-plt.figure(figsize=(6, 3), dpi = 200)
-plt.axhline(1000*smin.min(), color='r', linestyle='--', label='Min Sensitivity (%s mJy)' % round(1000*smin.min(), 2))
-plt.plot(freq, smin*1000, 'o-')
-plt.xlabel('Frequency (MHz)')
-plt.ylabel('Sensitivity Limit (mJy)')
-plt.title('Sensitivity limit for %s' % (trgt_name), fontsize=12)
-plt.legend()
-plt.savefig('./elevation-plots/%s-sensitivity-plot.png' % (trgt_name), dpi=200)
 plt.show()
